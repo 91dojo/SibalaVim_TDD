82330fc7fd7314bdddb4068c461be6b9a58b0e70
hatelove <jay.hatelove@gmail.com>
2017-09-26 11:49:24 +0800

    first failed test case: OneColor

---
+++ SibalaVim_TDD/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SibalaVim_TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OneColor()
+        {
+            var dice = new Dice(1, 1, 1, 1);
+            Assert.AreEqual("One Color", dice.Output);
+        }
+    }
+
+    public class Dice
+    {
+        public Dice(int i, int i1, int i2, int i3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Output { get; set; }
+    }
+}
