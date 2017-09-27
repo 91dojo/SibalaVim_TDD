2f8395273831eec34331904b73011070a92cc182
hatelove <jay.hatelove@gmail.com>
2017-09-27 17:40:38 +0800

    adjust one color test case with assertion MaxValue

--- SibalaVim_TDD/UnitTest1.cs
+++ SibalaVim_TDD/UnitTest1.cs
@@ -25,6 +25,7 @@ namespace SibalaVim_TDD
 
             if (IsOneColor())
             {
+                this.MaxPoint = _dices.First();
                 return "One Color";
             }
 
@@ -39,9 +40,11 @@ namespace SibalaVim_TDD
         private int GetPoints(IEnumerable<IGrouping<int, int>> diceGroups)
         {
             var pair = diceGroups.Where(x => x.Count() == 2).Min(x => x.Key);
-            this.MaxPoint = _dices.Where(x => x != pair).Max();
-            return _dices.Where(x => x != pair).Sum();
+            var points = _dices.Where(x => x != pair);
+            this.MaxPoint = points.Max();
+            return points.Sum();
         }
+
         private bool IsOneColor()
         {
             return this._dices.All(x => x == _dices.First());
@@ -76,13 +79,16 @@ namespace SibalaVim_TDD
         public void NormalPoints_4242_is_8_points()
         {
             DiceOutputShouldBe("8 Points", 4, 2, 4, 2);
+            MaxPointShouldBe(4);
         }
 
         [TestMethod]
         public void OneColor()
         {
             DiceOutputShouldBe("One Color", 1, 1, 1, 1);
+            MaxPointShouldBe(1);
         }
+
         private void DiceOutputShouldBe(string expected, int i0, int i1, int i2, int i3)
         {
             dice = new Dice(i0, i1, i2, i3);
