e197fe85d43bb85b0f187c4452d50c7fc9c91736
hatelove <jay.hatelove@gmail.com>
2017-09-27 17:32:44 +0800

    refactor: re-organized

--- SibalaVim_TDD/UnitTest1.cs
+++ SibalaVim_TDD/UnitTest1.cs
@@ -4,39 +4,6 @@ using System.Linq;
 
 namespace SibalaVim_TDD
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void OneColor()
-        {
-            DiceOutputShouldBe("One Color", 1, 1, 1, 1);
-        }
-
-        [TestMethod]
-        public void NoPoints()
-        {
-            DiceOutputShouldBe("No Points", 1, 2, 3, 4);
-        }
-
-        [TestMethod]
-        public void NormalPoints_1123_is_5_Points()
-        {
-            DiceOutputShouldBe("5 Points", 1, 1, 2, 3);
-        }
-
-        [TestMethod]
-        public void NormalPoints_4242_is_8_points()
-        {
-            DiceOutputShouldBe("8 Points", 4,2,4,2);
-        }
-        private static void DiceOutputShouldBe(string expected, int i0, int i1, int i2, int i3)
-        {
-            var dice = new Dice(i0, i1, i2, i3);
-            Assert.AreEqual(expected, dice.GetOutput());
-        }
-    }
-
     public class Dice
     {
         private List<int> _dices;
@@ -62,20 +29,54 @@ namespace SibalaVim_TDD
             return "No Points";
         }
 
-        private int GetPoints(IEnumerable<IGrouping<int, int>> diceGroups)
-        {
-            var pair = diceGroups.Where(x => x.Count() == 2).Min(x => x.Key);
-            return _dices.Where(x => x != pair).Sum();
-        }
-
         private static bool IsNormalPoints(IEnumerable<IGrouping<int, int>> diceGroups)
         {
             return diceGroups.Max(x => x.Count() == 2);
         }
 
+        private int GetPoints(IEnumerable<IGrouping<int, int>> diceGroups)
+        {
+            var pair = diceGroups.Where(x => x.Count() == 2).Min(x => x.Key);
+            return _dices.Where(x => x != pair).Sum();
+        }
         private bool IsOneColor()
         {
             return this._dices.All(x => x == _dices.First());
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private Dice dice;
+
+        [TestMethod]
+        public void NoPoints()
+        {
+            DiceOutputShouldBe("No Points", 1, 2, 3, 4);
+        }
+
+        [TestMethod]
+        public void NormalPoints_1123_is_5_Points()
+        {
+            DiceOutputShouldBe("5 Points", 1, 1, 2, 3);
+        }
+
+        [TestMethod]
+        public void NormalPoints_4242_is_8_points()
+        {
+            DiceOutputShouldBe("8 Points", 4, 2, 4, 2);
+        }
+
+        [TestMethod]
+        public void OneColor()
+        {
+            DiceOutputShouldBe("One Color", 1, 1, 1, 1);
+        }
+        private void DiceOutputShouldBe(string expected, int i0, int i1, int i2, int i3)
+        {
+            dice = new Dice(i0, i1, i2, i3);
+            Assert.AreEqual(expected, dice.GetOutput());
         }
     }
 }
