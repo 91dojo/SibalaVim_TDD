e62537032a3ccf2d6516613419a75b6de4bf1dc2
hatelove <jay.hatelove@gmail.com>
2017-09-27 15:35:22 +0800

    pass the test case: No Points

--- SibalaVim_TDD/UnitTest1.cs
+++ SibalaVim_TDD/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SibalaVim_TDD
 {
@@ -11,6 +11,12 @@ namespace SibalaVim_TDD
         public void OneColor()
         {
             DiceOutputShouldBe("One Color", 1, 1, 1, 1);
+        }
+
+        [TestMethod]
+        public void NoPoints()
+        {
+            DiceOutputShouldBe("No Points", 1, 2, 3, 4);
         }
 
         private static void DiceOutputShouldBe(string expected, int i0, int i1, int i2, int i3)
@@ -33,7 +39,12 @@ namespace SibalaVim_TDD
         {
             get
             {
-                return "One Color";
+                if (this._dices.All(x => x == _dices.First()))
+                {
+                    return "One Color";
+                }
+
+                return "No Points";
             }
         }
     }
