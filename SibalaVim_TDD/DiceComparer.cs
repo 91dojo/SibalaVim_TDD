f540a77b550aa4b5f5c17433d295005d655f5790
hatelove <jay.hatelove@gmail.com>
2017-09-28 13:24:47 +0800

    refactor: extract method CompareOfOneColor()

--- SibalaVim_TDD/DiceComparer.cs
+++ SibalaVim_TDD/DiceComparer.cs
@@ -11,14 +11,17 @@ namespace SibalaVim_TDD
                 if (dice1.Type == DiceType.OneColor)
                 {
                     //1 > 4 > 6 > 5 > 3 > 2
-                    var weights = new List<int> { 2, 3, 5, 6, 4, 1 };
-                    var weightOfDice1 = weights.IndexOf(dice1.Points); 
-                    var weightOfDice2 = weights.IndexOf(dice2.Points);
-                    return weightOfDice1 - weightOfDice2;
+                    return CompareOfOneColor(dice1, dice2);
                 }
                 return dice1.Points - dice2.Points;
             }
             return dice1.Type - dice2.Type;
+        }
+
+        private static int CompareOfOneColor(Dice dice1, Dice dice2)
+        {
+            var weights = new List<int> {2, 3, 5, 6, 4, 1};
+            return weights.IndexOf(dice1.Points) - weights.IndexOf(dice2.Points);
         }
     }
 }
