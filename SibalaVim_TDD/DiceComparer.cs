038c6fa9928b8ddf9b598cdcbc365483e61d968e
hatelove <jay.hatelove@gmail.com>
2017-09-28 11:56:36 +0800

    pass test case "both no points should be equal"  by hard-code

--- SibalaVim_TDD/DiceComparer.cs
+++ SibalaVim_TDD/DiceComparer.cs
@@ -4,13 +4,10 @@ namespace SibalaVim_TDD
 {
     internal class DiceComparer:IComparer<Dice>
     {
-        public DiceComparer()
-        {
-        }
 
         public int Compare(Dice dice1, Dice dice2)
         {
-            throw new System.NotImplementedException();
+            return 0;
         }
     }
 }
